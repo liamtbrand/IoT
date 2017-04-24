f200e017237e7b3e4a35aeb79b6648e6ae2eb7c3
Liam Brand <lbra112@aucklanduni.ac.nz>
2017-04-24 20:01:05 +1200

    Ignore node modules

---
+++ Hub/src/routing/Rule.java
@@ -0,0 +1,15 @@
+package routing;
+
+public class Rule {
+	
+	private String _trigger;
+	
+	public Rule(String trigger){
+		_trigger = trigger;
+	}
+	
+	boolean triggeredBy(){
+		return true; // TODO write all of this.
+	}
+
+}

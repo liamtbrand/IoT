4b200eded80a759935c5475ba3e013ea6b6b6eff
Liam Brand <lbra112@aucklanduni.ac.nz>
2017-04-25 11:31:34 +1200

    Update dependency installer

--- WebInterface/installDeps.sh
+++ WebInterface/installDeps.sh
@@ -2,9 +2,11 @@ curl --silent --location https://deb.nodesource.com/setup_6.x | sudo bash -
 sudo apt-get install --yes nodejs
 
 npm install socket.io
+npm install --save express
 
 npm install -g gulp
 
 npm install semantic-ui --save
 cd semantic/
 gulp build
+cd ../
